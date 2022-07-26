6bfadf0da97621f7952a183656e75011a2746269
stephanegirard <staifgirard@gmail.com>
2022-07-26 03:22:47 +0200

    amélioration de la boucle

--- php/ensalle.php
+++ php/ensalle.php
@@ -7,8 +7,6 @@
 
     <?php 
 
-        // On défini un tableau de films
-
         $films = [
             "Harry Potter à l'école des sorciers",
             "Divergente",
@@ -16,10 +14,7 @@
             "Wayne's World",
             "Le cinquième élément",
             "Un sixième élément"
-        ];
-
-        //var_dump($films);
-    
+        ];  
     ?>
 
     <h2 class="page__title">Page en salle</h2>
@@ -28,59 +23,33 @@
 
     <ul>
 
-    <?php
-        // On veut utiliser une boucle for pour
-        // parcourir le tableau et afficher les films
-
-        // for(condition initiale;condition finale; pas de la boucle)
-        // définir une variable qui va nous permettre
-        // de faire défiler un indice de tableau
-
-        // la variable $indice peut être nommée comme
-        // vous le souhaitez (count, i, etc.) !
-
-for ($indice = 0; $indice < 6; $indice ++) {
-    //var_dump($indice);
-
-    // par contre comment le for comprend que
-    // c'est le tableau précédent qu'il faut prendre ?
+        <?php
             
-    // Ici on parcours 6 fois la boucle,
-    // On veut donc pouvoir récupérer à chaque passage
-    // la valeur comprise dans la case qu'on est en train
-    // de parcourir
+            for ($indice = 0; $indice < 6; $indice ++) {
+        ?>
 
-    // Par exemple, $indice = 0
-    // Pour récupérer la valeur du film à la case 0
-    // $titre = $films[$indice]
-
-    //var_dump($films[$indice]);?>
-
-    <?php echo "<li>".$films[$indice]."</li>"; ?>
+        <?php echo "<li>".$films[$indice]."</li>"; ?>
+    </ul>
 
     <?php
         }
     ?>
 
-    </ul>
-
-</section>
-<section>
-    <?php 
-
-        // On définit un tableau de salles
-
-        $salles = [
-            "Athéna",
-            "Dyonisos",
-            "Hadès",
-            "Zeus"
+    <?php
+            // On définit un tableau de salles
+        $rooms = [
+            'Athéna',
+            'Dyonisos',
+            'Hadès',
+            'Zeus'
         ];
     ?>
-        <h3>Salles</h3>
-    
+
+    <h3>Voici la liste des salles : </h3>
+
     <ul>
-    <?php
+
+        <?php
         for ($ind = 0; $ind < 4; $ind ++) {
             ?>
     
@@ -88,15 +57,12 @@ for ($indice = 0; $indice < 6; $indice ++) {
 
             <?php
                 }
-            ?>
+        ?>
         
-            </ul>
+    </ul>
         
-        </section>
-        
-
-    
 </section>
+
 </main>
 
 <?php
